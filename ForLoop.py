748f887aa2a3f2c1911c8758d7ea4e748a411be3
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-05-30 18:22:18 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,22 @@
+#for loop
+for i in range(20):
+   print(i)
+   
+print("======")
+   
+#even no
+for i in range(0,21,2):
+    print(i)
+    
+print("======")    
+    
+#odd no
+for i in range(1,20,2):
+    print(i)
+    
+print("======")
+
+for i in range(5):
+   a=int(input("Enter a no : "))
+   b=int(input("Enter a no : "))   
+   print(a+b) 
