0b3e99aace9677500b773b17cd1207b1fcf7386a
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-05-30 18:22:44 +0530

    Add files via upload

---
+++ NestedLoop.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    for j in range(i):
+      print("*",end="")
+    print("")
+    
+print("--------")
+
+for i in range(5,0,-1):
+    for j in range(i):
+       print("*",end="")
+    print("")
+    
+print("--------")
+
+for i in range(65,70,1):
+    for j in range(64,70,1):
+       print(chr(j),end="")
+    print("")
+     
